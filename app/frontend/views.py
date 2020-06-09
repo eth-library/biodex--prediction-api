7e315c6cc68f49bf3a0b9a48cb57311bbd4618ea
BarryS <barry.sunderland@outlook.com>
2020-06-09 10:41:34 +0200

    display taxonomy counts on home page

--- app/frontend/views.py
+++ app/frontend/views.py
@@ -1,28 +1,25 @@
 from django.shortcuts import render
 
-from taxonomy.models import Family
+from taxonomy.models import Family, Subfamily, Genus, Species
 
 def home_view(request):
     """View function for home page of site."""
 
     # # Generate counts of some of the main objects
-    num_families = Family.objects.all().count()
-    # num_instances = BookInstance.objects.all().count()
-    
-    # # Available books (status = 'a')
-    # num_instances_available = BookInstance.objects.filter(status__exact='a').count()
-    
-    # # The 'all()' is implied by default.    
-    # num_authors = Author.objects.count()
+    num_family = Family.objects.all().count()
+    num_subfamily = Subfamily.objects.all().count()
+    num_genus = Genus.objects.all().count()
+    num_species = Species.objects.all().count()
 
     page_title = 'Home'
     
     context = {
         'page_title':page_title,
-        'num_families': num_families,
-        # 'num_instances': num_instances,
-        # 'num_instances_available': num_instances_available,
-        # 'num_authors': num_authors,
+        'num_family': num_family,
+        'num_subfamily': num_subfamily,
+        'num_genus': num_genus,
+        'num_species': num_species,
+
     }
 
     # Render the HTML template index.html with the data in the context variable
