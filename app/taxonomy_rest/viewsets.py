4f79638a5fc6eb657d9af11628b4c6df0c8e08c3
BarryS <barry.sunderland@outlook.com>
2020-06-09 10:40:01 +0200

    add query fields to taxonomy  url requests

--- app/taxonomy_rest/viewsets.py
+++ app/taxonomy_rest/viewsets.py
@@ -12,15 +12,13 @@ class FamilyViewset(viewsets.ModelViewSet):
     serializer_class = FamilySerializer
 
     def get_queryset(self):
-        # qs = super().get_queryset()
-        qs = Family.objects.all()
-        requested_family = self.request.query_params.get('name') #.lower()
 
-        print('requested_family', requested_family)
+        qs = Family.objects.all()
+        requested_family = self.request.query_params.get('family')
 
         if requested_family is not None:
             requested_family = str(requested_family)
-            return qs.filter(name__iexact=requested_family)
+            return qs.filter(name__icontains=requested_family)
       
         return qs
 
@@ -31,11 +29,38 @@ class SubfamilyViewset(viewsets.ModelViewSet):
     queryset = Subfamily.objects.all()
     serializer_class = SubfamilySerializer
 
+    def get_queryset(self):
+
+        qs = Subfamily.objects.all()
+        requested_subfamily = self.request.query_params.get('subfamily')
+
+        if requested_subfamily is not None:
+            requested_subfamily = str(requested_subfamily)
+            
+            return qs.filter(name__icontains=requested_subfamily)
+        
+        return qs
+
 class GenusViewset(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAdminUser]
 
     queryset = Genus.objects.all()
     serializer_class = GenusSerializer
+
+    def get_queryset(self):
+
+        qs = Genus.objects.all()
+        requested_genus = self.request.query_params.get('genus') #.lower()
+
+        print('requested_genus', requested_genus)
+
+        if requested_genus is not None:
+            requested_genus = str(requested_genus)
+            
+            return qs.filter(name__icontains=requested_genus)
+      
+        return qs
+
 
 class SpeciesViewset(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAdminUser]
@@ -60,18 +85,3 @@ class SpeciesViewset(viewsets.ModelViewSet):
             return qs.filter(parent=qs_genus_id)
       
         return qs
-
-    # def get_queryset(self):
-    #     """
-    #     Optionally restricts the returned species to a given genus,
-    #     by filtering against a `species` query parameter in the URL.
-    #     """
-    #     queryset = Species.objects.all()
-    #     genus_request = self.request.query_params.get('genus', None)
-
-    #     if genus_request is not None:
-    #         genus_key = Genus.objects.all().filter(genus__name=genus_request)
-    #         if len(genus_key)>0:
-    #             queryset = queryset.filter(species__parent=genus_key)
-
-    #     return queryset
