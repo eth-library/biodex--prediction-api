bd5c630162eaba998129c872915bd252f2a48119
BarryS <barry.sunderland@outlook.com>
2020-06-18 00:33:55 +0200

    update prediction view to use image byte stream

--- app/uploadforpredict_rest/views.py
+++ app/uploadforpredict_rest/views.py
@@ -20,46 +20,41 @@ def predict_image_view(request):
     """
     post an image for prediction
     """
+    
+    if request.method != 'POST':
+        return Response("only accepts POST Requests with the field 'image'", status=status.HTTP_400_BAD_REQUEST)
 
-    if request.method == 'POST':
+    elif request.method == 'POST':
         serializer = PredictImageSerializer(data=request.data)
 
         if serializer.is_valid():
             if DEBUG:
                 print('logging: serializer.is_valid')
+
             strt_time = time()
             serializer.save()
             #get prediction for posted image
             response_data = serializer.data
+
             if DEBUG:
                 request_string = str(request.data)
                 print(request_string)
-                im = Image.open(request.data['image'])
-                print(im)
+                img_stream = request.data['image'].open()
+                print('img_obj: ', type(img_stream))
 
             serialized_fname = os.path.basename(serializer.data['image'])
+            uploaded_img_path = MEDIA_ROOT + '/' +  serialized_fname                
+            predictions_data = get_prediction(img_stream, model_name=None)
 
-            uploaded_img_path = MEDIA_ROOT + '/' +  serialized_fname
+            if predictions_data:
+                pass
 
-            if FAKE_MODEL_RESPONSE:
-                response_data = {}
-                model_api_response = get_prediction(uploaded_img_path, model_name=None)
-
-                if model_api_response.status_code == 206:
-                    predictions_data = {'fake prediction':{'predictions':str([912,1,2,2,1,2,3,4])}}
-                
             else:
-                predictions_data = get_prediction(uploaded_img_path, model_name=None)
+                if DEBUG:
+                    print('logging: error with model prediction')
 
-                if predictions_data:
-                    pass
-
-                else:
-                    if DEBUG:
-                        print('logging: error with model prediction')
-
-                    #handle this error somehow # return HTTP bad response e.g.                        
-                    return Response('prediction model error', status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+                #handle this error somehow # return HTTP bad response e.g.                        
+                return Response('prediction model error', status=status.HTTP_500_INTERNAL_SERVER_ERROR)
             
             response_data['uploaded_image_saved_name']  = serialized_fname
 
@@ -68,6 +63,3 @@ def predict_image_view(request):
             response_data['exec_time'] = str(time() - strt_time) + ' s'
             
             return Response(response_data, status=status.HTTP_201_CREATED)
-            
-
-    return Response("only accepts POST Requests with the field 'image'", status=status.HTTP_400_BAD_REQUEST)
