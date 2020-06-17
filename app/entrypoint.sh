2d18b353bfa710da1e7506c98cc17888586b63d2
BarryS <barry.sunderland@outlook.com>
2020-06-18 00:30:49 +0200

    keep db  records on startup (don't flush)

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -2,22 +2,21 @@
 
 if [ "$DATABASE" = "postgres" ]
 then
-    echo "Waiting 15s for $DATABASE to start..."
+    "Waiting for $DATABASE to start..."
 
-    sleep 15 # using while loop with nc (netcat) and mysqladmin fails as packages are not found
+    sleep 5 # using while loop with nc (netcat) and mysqladmin fails as packages are not found
     
-    # while ! nc -z $SQL_HOST $SQL_PORT; do
-    # while ! mysqladmin ping -h"$SQL_HOST" --silent; do
-      # sleep .5
-    # done
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep .5
+    done
 
     echo "assuming $DATABASE has started"
 fi
 
 
-python manage.py flush --no-input
-python manage.py migrate
-python manage.py collectstatic --no-input --clear
-python3 manage.py createsuperuser
+# python manage.py flush --no-input
+# python manage.py migrate
+# python manage.py collectstatic --no-input --clear
+# python3 manage.py createsuperuser --no-input username=$DJANGO_SUPERUSER_USERNAME password=$DJANGO_SUPERUSER_PASSWORD EmailField=$DJANGO_SUPERUSER_EMAIL
 
 exec "$@"
