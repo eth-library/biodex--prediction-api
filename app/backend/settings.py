d5a028c7f39c80906510440dcbdef79d97301d86
BarryS <barry.sunderland@outlook.com>
2020-06-18 00:33:12 +0200

    remove default  django secret key

--- app/backend/settings.py
+++ app/backend/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-SECRET_KEY = os.environ.get("SECRET_KEY",'foo')
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
