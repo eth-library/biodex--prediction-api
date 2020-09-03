aa9b0fb108c76674008a319f0f4f38912c98ab7a
BarryS <barry.sunderland@outlook.com>
2020-09-03 16:33:31 +0200

    change to using standard nginx image and mounting the .conf file

--- podman/dev/build_images.sh
+++ podman/dev/build_images.sh
@@ -7,10 +7,6 @@ echo "building gunicorn-Django image"
 cd $ROOT_DIR/app
 buildah bud -t biodex/webapp-dev-img -f ./Dockerfile
 
-echo "building nginx image"
-cd $ROOT_DIR/nginx
-buildah bud -t biodex/nginx-prod-img -f ./Dockerfile
-
 echo "pulling postgres image"
 podman pull postgres:12.0-alpine
 
